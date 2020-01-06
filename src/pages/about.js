a54e4752db1fe9f0f53143dae1faeee8fa7ef81a
faalisco <ebrimafaal101@gmail.com>
2020-01-06 10:15:50 +0000

    inputed real content

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,19 +15,27 @@ class About extends React.Component {
                         About Us
                     </h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora dolorem atque nesciunt? Sit laboriosam velit officia aperiam, ab modi sunt necessitatibus provident, harum earum magni! Dolores quasi animi nostrum ut laboriosam
+                        VIRTIMMO est une agence immobilière qui est essentiellement au servir d’intermédiaire entre ceux qui sont à la recherche de logements et ceux qui en vendent ou en louent. Elle intervient  à toutes les étapes de la transaction immobilière, allant de la recherche du bien à la conclusion du contrat de vente ou de bail. Elle se positionne également  en conseiller pour éclairer les choix de ses clients. De part sa matricule de N.I.N.E.A: 007135933, sa profession est bien réglementée et reconnue par les services étatique d'avoir rempli les conditions nécessaires  pour exercer le métier  d'agence immobilière.
                     </p>
 
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora dolorem atque nesciunt? Sit laboriosam velit officia aperiam, ab modi sunt necessitatibus provident, harum earum magni! Dolores quasi animi nostrum ut laboriosam
+                        La détention de cette accréditation justifie que VIRTIMMO dispose des compétences requises pour effectuer des opérations immobilières au nom  de l'État ses clients.
                     </p>
 
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora dolorem atque nesciunt? Sit laboriosam velit officia aperiam, ab modi sunt necessitatibus provident, harum earum magni! Dolores quasi animi nostrum ut laboriosam
+                        Ses missions s’étendent aux transactions immobilières qu’effectuent, d’un côté, les vendeurs et les acheteurs, et de l’autre, les bailleurs et les locataires. Elle joue un rôle d’intermédiaire.
                     </p>
 
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora dolorem atque nesciunt? Sit laboriosam velit officia aperiam, ab modi sunt necessitatibus provident, harum earum magni! Dolores quasi animi nostrum ut laboriosam
+                        Dans le cas d’un particulier qui souhaite céder une maison ou un appartement, il peut confier la vente à VIRTIMMO. Celle-ci se chargera d’estimer la valeur du bien avant de publier des annonces. C’est à nous qu'il  revient de trouver des acquéreurs, de procéder aux visites de site et d’effectuer toutes les formalités relatives à la cession du bien.
+                    </p>
+
+                    <p>
+                        En outre, VIRTIMMO joue également un rôle de conseiller. Il peut faire bénéficier de son expertise dans différents domaines: technique, financier, juridique...
+                    </p>
+
+                    <p>
+                        En contrepartie des services que vous avez reçus de VIRTIMMO, vous devrez rémunérer notre agence qui a pris en charge la vente ou la location de votre bien. Ces frais d’agence peuvent être déterminés en fonction de la valeur du montant de la transaction.
                     </p>
 
                     <Agents />
